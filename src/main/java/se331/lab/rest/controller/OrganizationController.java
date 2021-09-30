8bf3979eb98db8ee89fa7c21a683af5bff355e8a
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-10-01 00:13:00 +0700

    3.2

--- src/main/java/se331/lab/rest/controller/OrganizationController.java
+++ src/main/java/se331/lab/rest/controller/OrganizationController.java
@@ -13,6 +13,8 @@ import se331.lab.rest.util.LabMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
+import org.springframework.http.HttpStatus;
+
 
 import java.util.List;
 
@@ -23,6 +25,17 @@ public class OrganizationController {
     @GetMapping("/organizers")
     ResponseEntity<?> getOrganizers(){
         return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()));
+    }
+
+    @GetMapping("organizers/{id}")
+    public ResponseEntity<?> getOrganizer(@PathVariable("id") Long id) {
+
+        Organizer output = organizerService.getOrganizer(id);
+        if (output != null) {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(output));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
     }
 
     @PostMapping("/organizers")
